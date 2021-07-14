8632ba4263bd42944f7ce5a0cf78a8b18269e713
Bruno Gomes <brunog.vix@gmail.com>
2021-07-14 05:08:32 -0300

    criado o time e tendo o sleep de 4 mili

--- client.py
+++ client.py
@@ -1,12 +1,29 @@
 import socket
 
+HEADERSIZE = 30
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(),1234))
 
-full_msg = ''
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
-print(full_msg)
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print("new msg len:",msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        print(f"tamanho da mensagem =>: {msglen}")
+
+        full_msg += msg.decode("utf-8")
+
+        print(len(full_msg))
+
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("caixa de entrada lotada")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ""
--- server.py
+++ server.py
@@ -1,13 +1,29 @@
-import socket;
+import socket
+import time
+# handle the hole message
+# 3,000,000,000 = 30 levando em consideração ao 
+# protocolo de que deve-se receber até 30 caracteres
 
-'''
+HEADERSIZE = 30
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(),1234))
 s.listen(5) ## fila 5
 
 while True:
-    clientsocket, adress = s.accept()
+    clientsocket, address = s.accept()
     print(f"Conexão com o {adress} estabilizada!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
-'''
+
+    msg = "Welcome to the server!"
+    msg = f"{len(msg):<{HEADERSIZE}}"+msg
+
+    clientsocket.send(bytes(msg,"utf-8"))
+
+    while True:
+        time.sleep(4)
+        msg = f"The time is {time.time()}"
+        msg = f"{len(msg):<{HEADERSIZE}}"+msg
+
+        print(msg)
+
+        clientsocket.send(bytes(msg,"utf-8"))
